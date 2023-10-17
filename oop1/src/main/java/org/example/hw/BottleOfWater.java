cf1ec21d77abdfb4f3653e719ba10be568641b35
Andrei <70104268+irre13@users.noreply.github.com>
2023-10-17 22:53:21 +0300

    Update BottleOfWater.java

--- oop1/src/main/java/org/example/hw/BottleOfWater.java
+++ oop1/src/main/java/org/example/hw/BottleOfWater.java
@@ -2,17 +2,7 @@ package org.example.hw;
 
 import java.math.BigDecimal;
 
-/**
- * Класс бутилированных напитков
- */
-public class BottleOfWater extends Drinkables {
-    /**
-     * Перегруженный конструктор класса Бутилированных напитков
-     *
-     * @param name   Наименование напитка
-     * @param price  Стоимость напитка
-     * @param volume Объем напитка
-     */
+public class BottleOfWater extends Drinkables {    
     public BottleOfWater(String name, BigDecimal price, Double volume) {
         super(name, price, volume);
     }
