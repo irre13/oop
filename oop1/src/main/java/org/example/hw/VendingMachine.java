ddc4cc20d0b8514100a4671a27bd6cf7e7a3fe50
Andrei <70104268+irre13@users.noreply.github.com>
2023-10-17 22:56:11 +0300

    Update VendingMachine.java

--- oop1/src/main/java/org/example/hw/VendingMachine.java
+++ oop1/src/main/java/org/example/hw/VendingMachine.java
@@ -1,18 +1,8 @@
 package org.example.hw;
 
-/**
- * Интерфейс торгового автомата
- */
 public interface VendingMachine {
-    /**
-     * Внесение продуктов в торговый автомат
-     */
+   
     void initProduct();
-
-    /**
-     * Получение продукта из торгового автомата
-     *
-     * @return строку
-     */
+    
     String getProduct();
 }
